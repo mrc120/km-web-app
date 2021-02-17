aed1e48326f40ff5dd5cfb64bff179e116eb6507
kristal <satrox120@gmail.com>
2021-02-17 13:08:07 +0100

    1

---
+++ react/src/index.js
@@ -0,0 +1,29 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/animate.min.css";
+import "./assets/scss/light-bootstrap-dashboard-react.scss?v=2.0.0";
+import "./assets/css/demo.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+import AdminLayout from "layouts/Admin.js";
+
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+    <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
+      <Redirect from="/" to="/admin/table" />
+          <App />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+
+
+serviceWorker.unregister();
